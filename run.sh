fd8838f987a608e9a56b478ad989b3c3f5c4920e
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-19 13:36:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ cd odious_working_dir
 python3 -m venv odiousenv
 source odiousenv/bin/activate
 pip install --upgrade pip
-pip install --upgrade odious
-for i in `seq 10000000000`; do python3 -c "import odious;from odious.matchup import comparison;comparison('235eda59cea66015679810130bd6e2de')" done
+while true; do
+    pip install --upgrade odious
+    python3 -c "import odious;from odious.matchup import comparison;comparison('235eda59cea66015679810130bd6e2de')"
+done
 
