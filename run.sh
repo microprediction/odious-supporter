e7ea99d21508bb6f31ae56a42c0f3a46fcc95b27
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-19 10:56:04 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+mkdir odious_working_dir
+cd odious_working_dir
+python3 -m venv odiousenv
+source odiousenv/bin/activate
+pip install --upgrade odious
+python3 -c "import odious;odious.matchup('235eda59cea66015679810130bd6e2de')"
