55b04ea32ff428c1338df3d36628b903f09682aa
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-01-19 23:22:23 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ python3 -m venv odiousenv
 source odiousenv/bin/activate
 pip install --upgrade pip
 while true; do
+    pip install --upgrade timemachines
     pip install --upgrade odious
     python3 -c "import odious;from odious.matchup import comparison;comparison('235eda59cea66015679810130bd6e2de')"
 done
